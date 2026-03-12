6bbeda89f92edf034bdbf5b694b0d1bf7f332119
abdelhamid <abdelhamaidm868@gmail.com>
2026-03-12 03:06:28 +0200

    user register

--- config/database.js
+++ config/database.js
@@ -7,10 +7,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  database: process.env.DB_NAME
 });
 
 
--- middleware/ay7aga.js
+++ middleware/ay7aga.js
@@ -3,7 +3,12 @@ console.log("goda");
 console.log("goda");
 for (let i =0 ; i<10 ;i++){
     console.log(i);
-    
 }
 console.log("goda");
 console.log("goda");
+
+
+
+
+
+
