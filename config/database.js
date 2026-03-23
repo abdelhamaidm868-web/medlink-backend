b6f9dd1bb5a49ada5005ea75f08bf020af62765e
abdelhamid <abdelhamaidm868@gmail.com>
2026-03-23 14:51:39 +0200

    before deployment2

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+//---------------------------------------------------
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
