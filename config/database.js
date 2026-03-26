00b87ee7212ad77b00d345d963bb676c93c195ca
abdelhamid <abdelhamaidm868@gmail.com>
2026-03-26 20:54:22 +0200

    55

--- config/database.js
+++ config/database.js
@@ -3,20 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 //---------------------------------------------------
- const db = mysql.createConnection({
+ const db = mysql.createConnection(
+  {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-});
+}
+);
 
 function DBconnection() {
   db.connect((error) => {
     if (error) {
       return console.log(error.message);
     } else {
-      return console.log("server connection with Database success");
+      return console.log("server connected with Database success");
     }
   });
 }
--- controllers/pharmacy.controller.js
+++ controllers/pharmacy.controller.js
@@ -225,3 +225,4 @@ export const getPharmacyOrders = (req, res) => {
     });
   });
 };
+
