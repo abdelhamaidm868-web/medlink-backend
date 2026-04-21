09623d62a7c19f7764e066ceca7daf49689fe3ab
Abdelrahman_goda <abdulrahmen1.lap@gmail.com>
2026-04-21 09:21:31 +0200

    initial commit

--- config/database.js
+++ config/database.js
@@ -2,27 +2,22 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
-//---------------------------------------------------
- 
-const db = mysql.createConnection({
+
+// ✅ بدل createConnection
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-}
-)
 
+  connectionLimit: 10,
+  waitForConnections: true,
+});
+
+// ❌ مش محتاج connect أصلاً
 function DBconnection() {
-  db.connect((error) => {
-    if (error) {
-      return console.log(error.message);
-      console.log("Database connection failed:", error.message);
-    } else {
-      return console.log("server connected with Database success");
-      console.log("Server connected with Database successfully");
-    }
-  });
+  console.log("Database pool is ready");
 }
 
 export { db, DBconnection };
